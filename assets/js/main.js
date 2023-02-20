fd80ff2aa4ce2fcf64487b3c8367e5ffecb2d816
Nazarii Luhovyi <luhovyinazarii@gmail.com>
2023-02-20 12:01:14 +0100

    init

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', function() {
+document.getElementById('burger').addEventListener('click', function () {
+  document.querySelector('header').classList.toggle('open');   
+})
+document.querySelector('.nav-list').addEventListener('click', function () {
+  document.querySelector('header').classList.toggle('open');
+})
+
+
+
+const allLangs = ['ru','ua','en','de'];
+let currentLang = 'de';
+const langButtons = document.querySelectorAll("[data-btn]");
+const currentPathName = window.location.pathname;
+let currentTextObject = {};
+
+const homeTexts = {
+  "h1_section-title": {
+    ru: "Луговой Назарий Ru",
+    ua: "Луговий Назарій Ua",
+    en: "Luhovyi Nazarii En",
+    de: "Luhovyi Nazarii De",
+  },
+};
+    
+// console.log(currentPathName);
+
+function checkPagePathName() {
+  switch (currentPathName) {
+    case "/index.html":
+      currentText = homeTexts;
+      break;
+    case "/another_page.html":
+      currentText = anotherTexts;
+      break;
+
+    default:
+      currentText = homeTexts;
+      break;
+  }
+}
+checkPagePathName();
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
